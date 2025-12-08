6961bc6093af211fcf6b028a7d6497d628072ab3
codeawy <codeawy@gmail.com>
2025-12-08 22:17:09 +0200

    Setup Zard UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ZardToastComponent } from '@shared/components/toast/toast.component';
+import { ZardButtonComponent } from '@shared/components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ZardToastComponent, ZardButtonComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('taskify');
